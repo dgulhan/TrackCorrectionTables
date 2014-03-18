8e79af8067b0030a046c3ed9546c4efe4138d3e0
Doga <dgulhan@cern.ch>
2014-03-18 22:48:53 +0100

    the ntupler I am using

--- example_macros/example.C
+++ example_macros/example.C
@@ -21,21 +21,18 @@
 #include "TCut.h"
 #include "TNtuple.h"
 #include "TLine.h"
-#include "/afs/cern.ch/user/d/dgulhan/workDir/missingPt/ntupler/trackTree.C"
-
-void example(){
+#include "../../trackEfficiency/ntupler/trackTree.C"
+ 
+void makeNtuple(){
  TH1D::SetDefaultSumw2();
  
- //input file
- //TString directory="/mnt/hadoop/cms/store/user/yenjie/HiForest_v27/";
- // TString infname="Dijet100_HydjetDrum_v27_mergedV1";
  TString algo="akVs3Calo";
- TString directory="root://eoscms//eos/cms/store/group/phys_heavyions/dgulhan";
- TString infname="HiForest_Pythia_Hydjet_Jet80_Track8_Jet19_STARTHI53_LV1_merged_forest_0";
- 
+ TString directory="/mnt/hadoop/cms/store/user/dgulhan/HIMC/MB/Track8_Jet21_STARTHI53_LV1/merged/";
+ TString infname="HiForest_HYDJET_Track8_Jet21_STARTHI53_LV1_merged_forest_0"; 
+  
  trackTree * ftrk = new trackTree(Form("%s/%s.root",directory.Data(),infname.Data()));
  HiTree * fhi = new HiTree(Form("%s/%s.root",directory.Data(),infname.Data()));
- t * fjet = new t(Form("%s/%s.root",directory.Data(),infname.Data()),algo);
+ t * fjet = new t(Form("%s/%s.root",directory.Data(),infname.Data()));
  
  //pt bins for track efficiency correction
 
@@ -45,7 +42,8 @@ void example(){
  
  int cent_min[]={  0, 10, 20, 30, 50, 0,10,20, 30, 50, 0,10, 20,  0};
  int cent_max[]={ 10, 20, 30, 50,100,10,20,30, 50,100,10,20,100,100};
- 
+   cout<<0<<endl;
+
  //getting histograms for track efficiency correction 
  TFile *f_eff[npt];
  TProfile *p_eff_cent[npt]; 
@@ -55,7 +53,7 @@ void example(){
  TFile *f_fake[npt];
  TProfile *p_fake_cent[npt]; 
  TProfile2D *p_fake_accept[npt]; 
- TProfile *p_fake_pt[npt]; 
+ TProfile *p_fake_pt[npt];  
  TProfile *p_fake_rmin[npt];  
  for(int ipt=0; ipt<npt;ipt++){
    f_eff[ipt]= new TFile(Form("../%s/eff/eff_pt%d_%d_cent%d_%d_%s_dogenjet0.root",algo.Data(),(int)ptmin[ipt],(int)ptmax[ipt],cent_min[ipt],cent_max[ipt],algo.Data()));
@@ -70,53 +68,104 @@ void example(){
    p_fake_rmin[ipt]=(TProfile*)f_fake[ipt]->Get("p_fake_rmin");
  }
 
- 
  //output file and tree
- TFile *outf= new TFile(Form("track_ntuple_%s_%s_testforfake.root",infname.Data(),algo.Data()),"recreate");
+ TFile *outf= new TFile(Form("MB.root",infname.Data(),algo.Data()),"recreate");
+ std::string particleVars="pt:matchedpt:eta:phi:rmin:trackselect:cent:eff";
 
- std::string trackVars="pt:eta:phi:rmin:trackselect:cent:eff:fake";
+ TNtuple *nt_particle = new TNtuple("nt_particle","",particleVars.data());
+ std::string trackVars="pt:eta:phi:rmin:trackselect:trackstatus:cent:eff:trkfake:fake";
+
  TNtuple *nt_track = new TNtuple("nt_track","",trackVars.data());
 
  //loop over events
  int nentries = ftrk->GetEntriesFast();
  // for(int jentry=0;jentry<nentries;jentry++){
- for(int jentry=0;jentry<100;jentry++){
+ for(int jentry=0;jentry<1000;jentry++){
   if((jentry%1000)==0) std::cout<<jentry<<"/"<<nentries<<std::endl;
-
   ftrk->GetEntry(jentry);
   fhi->GetEntry(jentry);
   fjet->GetEntry(jentry);
 
   float cent=fhi->hiBin;
   //loop over tracks
-  
-  for(int itrk=0;itrk<ftrk->nTrk;itrk++){
-   float trackselect=(ftrk->highPurity[itrk] && (ftrk->trkDxy1[itrk]/ftrk->trkDxyError1[itrk])<3.0 && (ftrk->trkDz1[itrk]/ftrk->trkDzError1[itrk])<3 && (ftrk->trkPtError[itrk]/ftrk->trkPt[itrk])<0.1);
-   float eta=ftrk->trkEta[itrk];
+  for(int itrk=0;itrk<ftrk->nParticle;itrk++){
 
-   if(fabs(eta)>2.4) continue; //acceptance of the tracker   
-   float pt=ftrk->trkPt[itrk];
-
-   if(pt<0.5) continue; 
-   float phi=ftrk->trkPhi[itrk];   
-   
+   float trackselect=(ftrk->mtrkQual[itrk] && fabs(ftrk->mtrkDxy1[itrk]/ftrk->mtrkDxyError1[itrk])<3.0 && fabs(ftrk->mtrkDz1[itrk]/ftrk->mtrkDzError1[itrk])<3 && (ftrk->mtrkPtError[itrk]/ftrk->mtrkPt[itrk])<0.1);
+   float eta=ftrk->pEta[itrk];
+   if(fabs(eta)>2.4) continue; 
+   float pt=ftrk->pPt[itrk];
+   float mpt=ftrk->mtrkPt[itrk];
+   float phi=ftrk->pPhi[itrk];
    float rmin=99;
-   
-   for(int ijet=0;ijet<fjet->ngen;ijet++){
-    if(fabs(fjet->geneta[ijet])>2 || fjet->genpt[ijet]<30) continue;
-    float r_reco=sqrt(pow(eta-fjet->geneta[ijet],2)+pow(acos(cos(phi-fjet->genphi[ijet])),2));
+ 
+
+   for(int ijet=0;ijet<fjet->nref;ijet++){
+    if(fabs(fjet->jteta[ijet])>2 || fjet->jtpt[ijet]<30) continue;
+    float r_reco=sqrt(pow(eta-fjet->jteta[ijet],2)+pow(acos(cos(phi-fjet->jtphi[ijet])),2));
     if(r_reco<rmin)rmin=r_reco;
    }
    
-   //get efficiency correction for the track   
-   float fake_pt,fake_cent,fake_accept,fake_rmin;
-   fake_pt=fake_cent=fake_accept=fake_rmin=0;
+   //get efficiency correction for the track
+   float eff_accept=1;
+   float eff_pt=1;
+   float eff_cent=1;
+   float eff_rmin=1;
+
+    for(int ipt=0;ipt<npt;ipt++){
+    if(pt>=ptmin[ipt] && pt<ptmax[ipt] && (0.5*cent)>=cent_min[ipt] && (0.5*cent)<cent_max[ipt]){
+      eff_pt=p_eff_pt[ipt]->GetBinContent(p_eff_pt[ipt]->FindBin(pt));
+      eff_cent=p_eff_cent[ipt]->GetBinContent(p_eff_cent[ipt]->FindBin(cent));
+      eff_accept=p_eff_accept[ipt]->GetBinContent(p_eff_accept[ipt]->GetXaxis()->FindBin(phi),p_eff_accept[ipt]->GetYaxis()->FindBin(eta));
+      if(rmin<5)eff_rmin=p_eff_rmin[ipt]->GetBinContent(p_eff_rmin[ipt]->FindBin(rmin));
+     }     
+   } 
+
+   float eff=1;
+   eff=eff_accept*eff_cent*eff_pt*eff_rmin;
+   if(eff==0){
+    cout<<"zero efficiency"<<" eta="<<eta<<" pt="<<pt<<" phi="<<phi<<" cent="<<cent<<endl;
+	  if(pt>100)eff=0.8;
+	  else eff=1;
+   }
+
+   //fill in the output tree
+
+   float entry[]={pt,mpt,eta,phi,rmin,trackselect,cent,eff};
+
+   nt_particle->Fill(entry);
+
+  }
+  
+  for(int itrk=0;itrk<ftrk->nTrk;itrk++){
+
+   float trackselect=(ftrk->highPurity[itrk] && fabs(ftrk->trkDxy1[itrk]/ftrk->trkDxyError1[itrk])<3.0 && fabs(ftrk->trkDz1[itrk]/ftrk->trkDzError1[itrk])<3 && (ftrk->trkPtError[itrk]/ftrk->trkPt[itrk])<0.1);
+   float eta=ftrk->trkEta[itrk];
+
+   if(fabs(eta)>2.4) continue;   
+   float pt=ftrk->trkPt[itrk];
+
+   if(pt<0.5) continue; 
+   float phi=ftrk->trkPhi[itrk];
+   float trkfake=ftrk->trkFake[itrk];
+   float trackstatus=ftrk->trkStatus[itrk];
+   float rmin=99;
+
+   for(int ijet=0;ijet<fjet->nref;ijet++){
+    if(fabs(fjet->jteta[ijet])>2 || fjet->jtpt[ijet]<30) continue;
+    float r_reco=sqrt(pow(eta-fjet->jteta[ijet],2)+pow(acos(cos(phi-fjet->jtphi[ijet])),2));
+    if(r_reco<rmin)rmin=r_reco;
+   }
+
+   //get efficiency correction for the track
+    float eff_accept=1;
+    float eff_pt=1;
+    float eff_cent=1;
+    float eff_rmin=1;
    
-   float eff_pt,eff_cent,eff_accept,eff_rmin;
-   eff_pt=eff_cent=eff_accept=eff_rmin=0;
-   
-   //given the pt,centrality,eta,phi and rmin of the track find the factorized efficiencies
-   for(int ipt=0;ipt<npt;ipt++){
+    float fake_pt,fake_cent,fake_accept,fake_rmin;
+    fake_pt=fake_cent=fake_accept=fake_rmin=0;
+
+    for(int ipt=0;ipt<npt;ipt++){
     if(pt>=ptmin[ipt] && pt<ptmax[ipt] && (0.5*cent)>=cent_min[ipt] && (0.5*cent)<cent_max[ipt]){
       eff_pt=p_eff_pt[ipt]->GetBinContent(p_eff_pt[ipt]->FindBin(pt));
       eff_cent=p_eff_cent[ipt]->GetBinContent(p_eff_cent[ipt]->FindBin(cent));
@@ -129,24 +178,25 @@ void example(){
       if(rmin<5)fake_rmin=p_fake_rmin[ipt]->GetBinContent(p_fake_rmin[ipt]->FindBin(rmin));      
      }     
    } 
-   
-   //multiply the factorized corrections to get the overall efficiency
    float eff=1;
    eff=eff_accept*eff_cent*eff_pt*eff_rmin;
    float fake=0;
-   if(pt<100)fake=fake_accept+fake_cent+fake_pt+fake_rmin;   //the if statements are temporary next corrections won't have these
-   if(eff==0){ //the if statements are temporary next corrections won't have these
+   if(pt<100)fake=fake_accept+fake_cent+fake_pt+fake_rmin;
+   if(eff==0){
     cout<<"zero efficiency"<<" eta="<<eta<<" pt="<<pt<<" phi="<<phi<<" cent="<<cent<<endl;
 	  if(pt>100)eff=0.8;
 	  else eff=1;
    }
+
    //fill in the output tree
-   float entry[]={pt,eta,phi,rmin,trackselect,cent,eff,fake};
+   float entry[]={pt,eta,phi,rmin,trackselect,trackstatus,cent,eff,trkfake,fake};
    nt_track->Fill(entry);
+
   }
    
  }
-  cout<<"writing to file"<<endl;
+ 
   nt_track->Write();
+  nt_particle->Write();
   outf->Close();
 }
